c5639cbc161da973c263f2be5d021b3d2982dfa4
huynhloi <huynhloi0910@gmail.com>
2023-05-25 08:09:08 +0700

    update date

--- js/slide.js
+++ js/slide.js
@@ -3,7 +3,7 @@
         let slider_content = `
         <span>WELCOME TO OUR BEGINNING</span>
         <h4>to have and to hold<br>from this day forward</h4>
-        <p>22 - 05 - 2023</p>
+        <p>06 - 06 - 2023</p>
         `
         $('.slider-content--inner').html(slider_content)
     })
